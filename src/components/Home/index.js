e6ddf29aed1d578c929ab4d8460689e8e2e0da74
Kevin Rama <ramanantsoa.vlm@gmail.com>
2022-07-26 18:25:52 +0200

    mise en place du CSS et de Grid

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,33 @@
 import styled from "styled-components";
 
+import GridButton from "./GridButton"
+
+const list = ["el1", "el2", "el3", "el4"];
+
 export default function Home() {
-    return <Wrapper><h2>La page Home</h2></Wrapper>
+    return (
+        <Wrapper>
+            <Grid>{list.map((el)=>{
+               return <GridButton key={el}>{el}</GridButton>;
+            })}</Grid>
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled.div``;
+
+const Grid = styled.div`
+display: grid;
+width: 400px;
+height: 400px;
+border: solid 1px;
+grid-template-columns: 1fr 1fr;
+padding: 24px;
+column-gap: 12px;
+row-gap: 12px;
+margin: auto;
+@media (max-width: 640px) {
+    width: 300px;
+    height: 300px;
+}
+`;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,19 @@
 import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom"
 
 export default function Header({isLight, handleToggleTheme}) {
-    return <Wrapper>
+    const location = useLocation();
+    return (
+    <Wrapper>
         <h1>Our super music app</h1>
+        <nav>
+            <Link to="/"><MenuEl isCurrentPage={location.pathname === "/"}>Home</MenuEl></Link>
+            <Link to="/about"><MenuEl isCurrentPage={location.pathname === "/about"}>About</MenuEl></Link>
+        </nav>
         <button onClick={handleToggleTheme}>Switch to {isLight ? "dark" : "light"} theme</button>
     </Wrapper>
-};
+    );
+}
 
 const Wrapper = styled.header`
     height: 80px;
@@ -14,4 +22,17 @@ const Wrapper = styled.header`
     padding: 0px 24px;
     align-items: center;
     border-bottom: solid 1px;
+    & a {
+        text-decoration: none;
+        color: inherit;
+        margin: 15px;
+    }
+`;
+
+const MenuEl = styled.p`
+padding-bottom: 2px;
+border-bottom: solid 2px ${props=>props.isCurrentPage ? "" : "transparent"};
+&:hover {
+    border-bottom: solid 2px
+}
 `;
